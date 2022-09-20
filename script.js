f2195f82102da3cb069f553e27ce28cef6ebb4fb
r4vsdev <r4vsdev@gmail.com>
2022-09-20 20:39:15 -0300

    Add a button to make a new grid with the specified size

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+let size;
 function makeGrid(numberOfSquares) {
-    for (let i=0 ; i <= numberOfSquares ; i++) {
+    for (i=1 ; i <= numberOfSquares**2 ; i++) {
         const grid = document.querySelector('.grid');
         const square = document.createElement('div');
-        square.classList.add('square')
-        square.textContent = i
+        square.classList.add('square');
+        square.textContent = i;
         grid.appendChild(square);
+    }
+    size = i-1;
+}
+
+function deleteGrid() {
+    for (i = 1 ; i <= size ; i++) {
+        const square = document.querySelector('.square');
+        square.remove();
     }
 }
 
-makeGrid(256)
+function changeBackground() {
+    this.style.background = 'white';                                      
+}
 
+function getNewSize () {
+    let newSize = window.prompt("Enter the new size of the grid","16");
+    deleteGrid();
+    makeGrid(newSize);
+}
+
+
+makeGrid(16)
+
+// change background on hover
 const squares = document.querySelectorAll('.square')
 squares.forEach(square => square.addEventListener('mouseenter',changeBackground ));
 
-function changeBackground() {
-    console.log('hi');
-    this.style.background = 'white';                                      
-
-}
-
+// New Grid
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', getNewSize);
