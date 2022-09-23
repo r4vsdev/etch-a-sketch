2bb736b922879244175c843f607345b5f2d3713d
r4vsdev <r4vsdev@gmail.com>
2022-09-22 23:11:58 -0300

    Improved UI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let size;
 let squares;
+let currentColor = 0; // 0 maps to black, 1 maps to colorful
 function makeGrid(numberOfSquares) {
     for (i=1 ; i <= numberOfSquares**2 ; i++) {
         const grid = document.querySelector('.grid');
@@ -12,7 +13,7 @@ function makeGrid(numberOfSquares) {
     }
     size = i-1;
     updateSquareList();
-    squares.forEach(square => square.addEventListener('mouseenter', changeBackground ));
+    squares.forEach(square => square.addEventListener('mouseenter', changeBackgroundToWhite ));
 }
 function deleteGrid () {
     for (i = 1 ; i <= size ; i++) {
@@ -24,12 +25,14 @@ function getRandomColor () {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     return `#${randomColor}`;
 }
-function changeBackground () {
-    // this.style.background = getRandomColor();
+function changeBackgroundToWhite () {
     this.style.background = 'white';
 }
+function changeBackgroundToRandom () {
+    this.style.background = getRandomColor();
+}
 function makeNewGrid () {
-    let newSize = +window.prompt("Enter the new size of the grid","10");
+    let newSize = +window.prompt("Enter the new size of the grid","20");
     if (newSize > 50) {newSize = 50;}
     if (newSize <= 0) {newSize = 4}
     deleteGrid();
@@ -39,8 +42,13 @@ function updateSquareList () {
     squares = document.querySelectorAll('.square');
 }
 
-makeGrid(4);
+makeGrid(10);
 
 // New Grid btn
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', makeNewGrid);
+
+
+// black
+// colorful
+
